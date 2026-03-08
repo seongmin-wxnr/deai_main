79b66a655e8b1bf824e7aeead5e1179f43df3b6c
seongmin-wxnr <yuilove9105@gmail.com>
2026-03-08 17:50:14 +0900

    Update asgi.py

--- deai/asgi.py
+++ deai/asgi.py
@@ -1,18 +1,24 @@
+"""
+ASGI config for deai project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-import deai_project.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from deai_project.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deai.settings')
-
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            deai_project.routing.websocket_urlpatterns
-        )
+application = ProtocolTypeRouter({ 
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
     ),
 })
